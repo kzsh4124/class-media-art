5dbcd40703e5cc28833f882c235a848a12823c49
Kazushi Nakamura <49474508+kzsh4124@users.noreply.github.com>
2022-08-02 10:51:36 +0900

    commit changes

--- app/static/shader_template.frag
+++ app/static/shader_template.frag
@@ -16,10 +16,33 @@ vec3 ring(vec2 origin, vec2 center, float r, float intensity, vec3 color){
 	return color*intensity / abs(length(origin - center) - r);
 }
 //中央0の正規化座標
-vec2 center_zero(void){
+vec2 center_zero(vec2 r){
 	return (gl_FragCoord.xy*2.0 - r)/ min(r.x, r.y);
 }
+// wave
+vec3 wave(vec2 p, vec2 center, vec3 color){
+	float dist = length(p - center);
+	return color * sin(dist);
+}
 
+//distance_source: , distort_source: , distance_center: 
+
+vec2 GetRippleDistortedUv(float distance_source_u, float distance_source_v, float distort_source_u, float distort_source_v, float distance_center_x,float distance_center_y){
+    float distance_square = (distance_source_u - distance_center_x) * (distance_source_u - distance_center_x)
+        + (distance_source_v - distance_center_y) * (distance_source_v - distance_center_y);
+    float distance = sqrt(distance_square);
+    float sine_disappear_distance = u_ripple_sine_disappear_distance;
+    float normalized_distance = clamp(distance, 0.0, sine_disappear_distance) / sine_disappear_distance;
+    float sine_strength = u_ripple_strength * (1.0 - normalized_distance) * (1.0 - normalized_distance);
+    float theta = sin(u_ripple_offset + distance * u_ripple_frequency) * sine_strength
+    float u0 = distort_source_u - 0.5;
+    float v0 = distort_source_v - 0.5;
+    float u1 = u0 * cos(theta) - v0 * sin(theta);
+    float v1 = u0 * sin(theta) + v0 * cos(theta);
+    float u2 = u1 + 0.5;
+    float v2 = v1 + 0.5;
+    return vec2(u2, v2);
+}
 void main(void){
 	vec2 p = center_zero();
 	
--- app/static/sketch.js
+++ app/static/sketch.js
@@ -2,8 +2,16 @@ let theShader;
 let path_dir = "../static/";
 let backbuffer;
 let canvas;
-let frog_flag;
-let wave_flag;
+let frog_param = 0;
+let frog =0;
+let frog_time = 0.0;
+let anti_frog = 0;
+let wave_param = 0.0;
+let mic;
+let threshold;
+let wave_coord=[0,0];
+let wave_start=[0.0,0.0,0.0,0.0];
+let wave_inten=[0.0,0.0,0.0,0.0];
 function preload(){
     theShader = loadShader(path_dir+"shader.vert", path_dir+"shader.frag");
 }
@@ -13,22 +21,66 @@ function setup() {
     backbuffer = createGraphics(width, height, WEBGL);
     backbuffer.clear();
     noStroke();
+    mic = new p5.AudioIn();
+    mic.start();
+    threshold = 0.5;
+
 }
 function draw() {
-    backbuffer.clear();
-    backbuffer.image(canvas, width*(-0.5), height*(-0.5), width, height);
-    clear();
+    let now = millis()/1000;
     shader(theShader);
-    
-    theShader.setUniform("backbuffer", backbuffer);
+    //音量を取得
+    let vol = mic.getLevel();
+    if(vol > threshold){
+        wave_param += vol;
+        anti_frog += 1;
+    }else{
+        frog_param += 1;
+    }
+    if (anti_frog > 60){
+        frog_param = 0;
+        anti_frog = 0;
+    }
+    //定例的な波を生成
+    if ((frameCount+1) % 60 == 0){
+        for(let i=0;i<4;i++){
+            //その1秒の音量を送り、波を
+            if(wave_start[i] == 0.0){
+                wave_start[i] = now;
+                wave_inten[i] = wave_param;
+                wave_param = 0.0;
+            }
+            //描画から3秒以上経過した波は消す
+            else if(now - wave_start[i] >= 3.0){
+                wave_start[i] = 0.0;
+                wave_inten[i] = 0.0;
+            }
+        }
+    }
+    //カエル飛び込みのスタート
+    if(frog_param > 120 && frog_time == 0.0){
+        frog = 1;
+        frog_time = now;
+        frog_param = 0;
+    }
+    if(now-frog_time >= 4.0) {
+        frog = 0;
+        frog_time = 0.0;
+    }
+    theShader.setUniform("frog", frog);
+    theShader.setUniform("frog_time", frog_time);
+    theShader.setUniform("wave_start", wave_start);
+    theShader.setUniform("wave_inten", wave_inten);
     theShader.setUniform("u_resolution", [width, height]);
     theShader.setUniform("u_time", millis() / 1000.0);
     theShader.setUniform("mouse", [mouseX/width,map(mouseY, 0, height, 1, 0)]);
     theShader.setUniform("pixel_density", [pixelDensity()]);
-    console.log(backbuffer);
     rect(0, 0, width, height);
     
 }
 function windowResized(){
     resizeCanvas(windowWidth, windowHeight);
+}
+function randint(a,b){
+    return Math.floor(Math.random()*(b-a+1))+a;
 }
